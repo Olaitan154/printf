fe75f987ab720b3e1cf4bd548150f07201e39840
Balogun moshood olaitan <101404208+Olaitan154@users.noreply.github.com>
2022-05-25 22:04:51 +0100

    file added

--- _printf.c
+++ _printf.c
@@ -1,43 +1,49 @@
 #include "main.h"
+
 /**
- * _printf - printf function
- * @format: const char pointer
- * Return: b_len
+ *_printf - Print a formatted string
+ *@format: format string
+ *Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	int count = 0;
+	va_list list;
+	char *pointer, *start;
+	param_func flags = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-	register int count = 0;
+	va_start(list, format);
 
-	va_start(arguments, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (p = format; *p; p++)
+	for (pointer = (char *)format; *pointer; pointer++)
 	{
-		if (*p == '%')
+		init_params(&flags, list);
+		if (*pointer != '%')
 		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
+			count += _putchar(*pointer);
+			continue;
+		}
+		start = pointer;
+		pointer++;
+		while (get_flags(pointer, &flags))
+		{
+			pointer++;
+		}
+		pointer = get_width(pointer, &flags, list);
+		pointer = get_precision(pointer, &flags, list);
+		if (get_mods(pointer, &flags))
+			pointer++;
+
+		if (!func_parse(pointer))
+			count += print_range(start, pointer,
+			flags.l_mod || flags.h_mod ? pointer - 1 : 0);
+		else
+			count += print_func(pointer, list, &flags);
 	}
 	_putchar(-1);
-	va_end(arguments);
+	va_end(list);
 	return (count);
 }
